67071984b8f21eb84911653dc1e5468274e51f17
ostfars <v_minin@rambler.ru>
2023-02-08 20:25:08 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([44.9495, 34.1059], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
